bced8d7882fc0654f77f6a9e6390dd03e56e71e7
junseub.lim <junseub.lim@linecorp.com>
2024-08-30 20:26:43 +0900

    feat: init

---
+++ src/instrument.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://35c030ae2ed74a17865c832e9b019b3c@o912826.ingest.us.sentry.io/4505362630311936",
+  integrations: [
+    // See docs for support of different versions of variation of react router
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
